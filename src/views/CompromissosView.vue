62f79323b26a4ccb6d4947b745ab6c782c2294e9
ZTX7 <sineziosrjunior@gmail.com>
2025-05-17 18:23:17 -0300

    refact: refact_front_compromissos

--- src/views/CompromissosView.vue
+++ src/views/CompromissosView.vue
@@ -1,8 +1,47 @@
 <template>
-  <div class="calendar-container">
-    <FullCalendar :options="calendarOptions" />
-  </div>
-  <button @click="handleLogout" class="logout-button">LOGOUT</button>
+
+
+<body>
+    <section id="rgcolumn">
+
+
+      <div id="logodiv">
+        <img src="../assets/logowhite.png" alt="logomarca do projeto">
+        <div id="textlogodiv" class="textlogodiv">
+          <h3 id="h3sec1" class="textdivs1">Agenda</h3>
+          <h1 id="h1sec1" class="textdivs1">de Compromissos</h1>
+        </div>
+      </div>
+
+      <h2> Logado como: Negão. </h2>
+      <div class="little"> 
+        <h3 class="textlittle">Calendário</h3>
+        <div class="littlebox">
+
+        </div>
+      </div>
+
+      <div class="little"> 
+        <h3 class="textlittle">Mais importantes</h3>
+        <div class="littlebox">
+          
+        </div>
+      </div>
+
+      <button @click="handleLogout" class="logout-button">LOGOUT</button>
+
+    </section>
+
+  <section id="calendar">
+    <div class="calendar-container">
+      <FullCalendar :options="calendarOptions" />
+    </div>
+  </section>
+  <section id="secrender" class="oculto">
+      <button @click="handleLogout" id="button-logout" class="oculto">LOGOUT</button>
+  </section>
+
+</body>
 </template>
 
 <script setup lang="ts">
@@ -102,6 +141,62 @@ onMounted(() => {
 
 <style scoped>
 
+
+body {
+  display: flex;
+  flex-direction: row;
+  height: 100%;
+  width: 100%;
+  background-color: ;
+}
+
+  #rgcolumn {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+    height: rem;
+    width: 45rem;
+    background-color: #0D0C0C;
+  }
+
+  #logodiv {
+    margin-top: 2rem;
+}
+h2{
+  width: 80%;
+  margin: 2rem;
+  color: white;
+}
+#calendar{
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color:#1B1B1B;
+}
+
+.little{
+  display: flex;
+  flex-direction: column;
+  width: 80%;
+  height: 20rem;
+  margin: 2rem;
+  color: white;
+}
+
+.textlittle {
+  padding-left: 1rem;
+  font-size: 1.4rem;
+  padding-bottom: 0.3rem;
+}
+
+.littlebox{
+  background-color: #1D1D1D;
+  width: auto;
+  height: 100%;
+}
+
  .logout-button{
     font-size: 1.2rem;
     padding: 0 1rem;
@@ -113,18 +208,86 @@ onMounted(() => {
     height: 50px;
     transition: 0.5s;
     font-weight: 500;
-  }.logout-button:hover{
+    margin: 3rem;
+  }
+  
+  .logout-button:hover{
     transition: 0.5s;
     box-shadow: rgb(255, 198, 11) 0px 0px 5px 0px;
     cursor: pointer;
     transform: scale(1.03);
   }
 
+  
+
 .calendar-container {
   width: 100%;
   height: calc(100vh - 120px); /* Ajuste conforme necessário */
   padding: 10px;
   box-sizing: border-box;
+}
+
+.oculto{
+  display: none;
+}
+
+@media screen and (max-width: 950px){
+  body{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #rgcolumn{
+    width: 100%;
+    padding-bottom: 4rem;
+  }
+  #logodiv{
+    display: flex;
+    width: 80%;
+  }
+  .logout-button{
+    display: None;
+  }
+  #calendar{
+  display: flex;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  background-color:#1B1B1B;
+  }
+
+  
+  .oculto{
+    display: flex;
+    flex-direction: column;
+    justify-items: center;
+    height: 12vh;
+  }
+
+  button{
+    justify-content: center;
+    font-size: 1.2rem;
+    width: 80%;
+    padding: 0 1rem;
+    background-color: rgb(0, 0, 0);
+    color: white;
+    border: none;
+    border: 2px white solid;
+    border-radius: 20px;
+    height: 50px;
+    transition: 0.5s;
+    font-weight: 500;
+    margin: 3rem;
+  }
+
+  button:hover{
+    transition: 0.5s;
+    box-shadow: rgb(255, 198, 11) 0px 0px 5px 0px;
+    cursor: pointer;
+    transform: scale(1.03);
+  }
+
 }
 
 :deep(.fc-toolbar-title) {
@@ -174,4 +337,6 @@ onMounted(() => {
   color: #e3e3e3 !important;
   text-transform: capitalize !important;
 }
+
+
 </style>
